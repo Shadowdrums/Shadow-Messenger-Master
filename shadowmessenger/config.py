d4e26c20c272cd88745f0c5b9fa819c6b7266920
Shadowdrums <122622018+Shadowdrums@users.noreply.github.com>
2024-07-28 00:17:48 -0400

    Delete shadowmessenger directory

--- shadowmessenger/config.py
+++
@@ -1,5 +0,0 @@
-class MessengerConfig:
-    def __init__(self, keep_alive_interval = 10, timeout = 30) -> None:
-        self.KEEP_ALIVE_INTERVAL = keep_alive_interval
-        self.TIMEOUT = timeout
-        super().__init__()
